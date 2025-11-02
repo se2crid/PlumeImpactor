cee990ef01f09b3575b9f7dcc31e4be926a6c6bd
khcrysalis <sam4r16@gmail.com>
2025-11-02 09:15:18 -0800

    linux deps

--- apps/plumeimpactor/src/keychain.rs
+++ apps/plumeimpactor/src/keychain.rs
@@ -1,7 +1,7 @@
-#[cfg(all(target_os = "macos", not(debug_assertions)))]
-pub use keyring_core::Entry;
-#[cfg(not(all(target_os = "macos", not(debug_assertions))))]
-pub use keyring::Entry;
+// #[cfg(all(target_os = "macos", not(debug_assertions)))]
+// pub use keyring_core::Entry;
+// #[cfg(not(all(target_os = "macos", not(debug_assertions))))]
+// pub use keyring::Entry;
 
 const KEYRING_SERVICE: &'static str = "Plume Impactor Credentials";
 const KEYRING_USER: &'static str = "Apple ID";
